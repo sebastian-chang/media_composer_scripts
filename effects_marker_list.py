8413ea378f950a2df2134ab39f040e33d5abdfeb
Sebastian Chang <schang@Sebastians-Tardis.local>
2019-08-30 16:50:15 -0500

    Changed to PyQt5 instead of tkinter

--- effects_marker_list.py
+++ effects_marker_list.py
@@ -1,19 +1,12 @@
+from PyQt5.QtWidgets import (QApplication, QWidget, QComboBox, QGroupBox, QFormLayout,
+                             QLabel, QDialogButtonBox, QVBoxLayout, QDialog, QSpinBox, 
+                             QFileDialog, QLineEdit)
+from PyQt5.QtGui import QIcon
 import os
 import sys
 import pandas as pd
-import tkinter as tk
-import tkinter.filedialog
-import tkinter.ttk
 
-# Get file path to the effects list text file.
-def get_file_path():
-    user_info.destroy()
-    my_file_types = [('text files', '.txt')]
-    user_filepath = tk.filedialog.askopenfilename(parent = user_input_window, \
-                title = 'Please choose an effect list file:', filetypes = my_file_types, \
-                initialdir=os.path.expanduser('~/Desktop'))
-    make_marker_list(user_filepath)
-
+# Validates given track number as well as formats for Media Composer by adding 'V'
 def validate_track_number(track_name):
     try:
         temp_number = int(track_name)
@@ -23,28 +16,33 @@ def validate_track_number(track_name):
     return(track)
 
 # Takes the file given by user along with the users parameters to create a new markers list.
-def make_marker_list(filepath):
-    user_input_window.destroy()
+def make_marker_list(user_input):
+    file_path = user_input.file_path
+    marker_name = user_input.marker_name.text()
+    effect_name = user_input.effect_name.text()
+    marker_color = user_input.marker_color.currentText()
+    track_num = user_input.track_num.text()
+    
     effects_list = []   # Empty list that will be filled with matching effect name from user given file.
     in_effects_list = False
-    track = validate_track_number(track_name.get())
+    track = validate_track_number(track_num)
     
     # Split file name and file path to create the new file name in the same location.
-    effects_file_path = os.path.dirname(filepath)
-    marker_file_name = os.path.basename(filepath).split('.', 1)[0] + '-' + effect_name.get() + '.txt'
+    effects_file_path = os.path.dirname(file_path)
+    marker_file_name = os.path.basename(file_path).split('.', 1)[0] + '-' + effect_name + '.txt'
     marker_file_path = os.path.join(effects_file_path, marker_file_name)
 
     # Dummy proofs non input entry from users.
-    if(marker_name.get() == ''):
-        marker_name.set('Marker')
-    if(effect_name.get() == ''):
-        effect_name.set('No effect was given')
+    if(marker_name == ''):
+        marker_name = 'Marker'
+    if(effect_name == ''):
+        effect_name = 'No effect was given'
     # Opens file and checks for effect name in each line from file.
-    with open(filepath) as fp:
+    with open(file_path) as fp:
         for line in fp:
             if 'Effect Location Summary:' in line:
                 in_effects_list = False
-            if effect_name.get().lower() in line.lower() and in_effects_list == True:
+            if effect_name.lower() in line.lower() and in_effects_list == True:
                 effects_list.append(line.lstrip())
             if '  __ TRACK __  __ START TC __  ___ END TC ___    __________ EFFECT NAME __________' in line:
                 in_effects_list = True
@@ -58,8 +56,8 @@ def make_marker_list(filepath):
 
         marker_df = pd.DataFrame(marker_list)  
         marker_df.columns = ['track', 'color', 'start_tc', 'end_tc', 'comment'] # Rename columns to appropriate name.
-        marker_df.loc[:, 'marker_name'] = marker_name.get()
-        marker_df.loc[:, 'color'] = marker_colors[marker_choice.get()]
+        marker_df.loc[:, 'marker_name'] = marker_name
+        marker_df.loc[:, 'color'] = marker_color
         # Reorder columns to correct order for Media Composer
         marker_df = marker_df[['marker_name', 'start_tc', 'track', 'color', 'comment']] 
         marker_df.sort_values('start_tc', inplace = True)   # Sort by earliest timecode
@@ -71,39 +69,67 @@ def make_marker_list(filepath):
         # Media Composer doesn't need that information.
         marker_df.to_csv(marker_file_path, index = False, header = False, sep = '\t')
     else:
-        return(f'No effect was found in {filepath}')
+        return(f'No effect was found in {file_path}')
+
+# Create the PyQt5 class object for user input.
+class User_Input(QDialog):
+    def __init__(self):
+        super(User_Input, self).__init__()
+        self.createFormGroupBox()
+        
+        # Create our buttons
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        buttonBox.accepted.connect(self.showDialog)
+        buttonBox.rejected.connect(self.reject)
+        
+        # Set our widget layout
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.formGroupBox)
+        mainLayout.addWidget(buttonBox)
+        self.setGeometry(50, 50, 0, 0)
+        self.setLayout(mainLayout)
+        
+        self.setWindowTitle('Marker List Info')
+        
+    # Main user input form    
+    def createFormGroupBox(self):
+        self.marker_color = QComboBox(self)
+        self.track_num = QSpinBox()
+        self.marker_name = QLineEdit()
+        self.effect_name = QLineEdit()
+        
+        for color in marker_colors:
+            self.marker_color.addItem(color) # Add marker colors to our drop menu
+        self.track_num.setMinimum(1)
+        self.track_num.setMaximum(99)
+            
+        self.formGroupBox = QGroupBox("Form layout")
+        layout = QFormLayout()
+        layout.addRow(QLabel("Marker Name:"), self.marker_name)
+        layout.addRow(QLabel('Effect Name:'), self.effect_name)
+        layout.addRow(QLabel("Marker Color:"), self.marker_color)
+        layout.addRow(QLabel("Video Track:"), self.track_num)
+        self.formGroupBox.setLayout(layout)
+        self.show()
+        
+    # Get the file location from user 
+    def showDialog(self):
+        self.hide()
+        fname = QFileDialog.getOpenFileName(self, 'Open File', os.path.expanduser('~/Desktop'), 'Text Files (*.txt);; All Files (*.*)')
+        
+        if fname[0]:
+            self.file_path = fname[0]
+            make_marker_list(self)
 
 # Media Composer marker colors.
 marker_colors = ['Red', 'Blue', 'Green', 'Cyan', 'Yellow', 'Magenta', 'Black', 'White']
 
-# Create tkinter window and variables
-user_input_window = tk.Tk()
-user_input_window.title('Effects marker list')
-user_info = tk.ttk.Frame(user_input_window)
-effect_name = tk.StringVar()
-effect_name.set('')
-marker_choice = tk.IntVar()
-marker_name = tk.StringVar()
-marker_name.set('')
-track_name = tk.StringVar()
-track_name.set('')
-marker_choice.set(0)
+if __name__ == '__main__':
 
-user_info.pack()
-tk.ttk.Label(user_info, text = 'Please choice a color for you markers.').grid()
-
-# Loop to create radio button for all Media Composer marker colors.
-for position, color in enumerate(marker_colors):
-    tk.ttk.Radiobutton(user_info, text = color, variable = marker_choice, value = position).grid(sticky = tk.W)
-
-tk.ttk.Label(user_info, text = 'Name of the effect you are looking for:').grid()
-tk.ttk.Entry(user_info, textvariable = effect_name).grid(sticky = tk.W)
-tk.ttk.Label(user_info, text = 'Please enter a name for your markers:').grid()
-tk.ttk.Entry(user_info, textvariable = marker_name).grid(sticky = tk.W)
-tk.ttk.Label(user_info, text = 'Please enter a track for your markers:').grid()
-tk.ttk.Entry(user_info, textvariable = track_name).grid(sticky = tk.W)
-tk.ttk.Button(user_info, text = 'Cancel', command = user_input_window.destroy).grid(sticky = tk.W, row = 99, column = 0)
-tk.ttk.Button(user_info, text = 'Okay', command = get_file_path).grid(sticky = tk.W, row = 99, column = 1)
-
-
-user_input_window.mainloop()
+    # app = QApplication(sys.argv)
+    marker_app = QApplication.instance() # checks if QApplication already exists
+    if not marker_app: # create QApplication if it doesnt exist
+        marker_app = QApplication(sys.argv)
+        # marker_app = QWidget.QApplication(sys.argv)
+    ex = User_Input()
+    marker_app.exec_()
